5c9ae28fad96517d3b3bfe18d69fadebd085eb1c
Jeff Fredrickson <jeff.fredrickson@gmail.com>
2018-06-01 12:20:23 -0400

    add note about setting password first

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Convenient script to ssh into an instance, while setting up port forwarding
+# to localhost for accessing the Traefik dashboard.
+#
+# This script should only be used after initially setting the password.
+# Otherwise, the connection will fail due to being unable to initiate the port
+# forwarding.
+
 SSH_PORT=`grep "sshd_port:" configuration/vars.yml | awk '{print $2}'`
 TRAEFIK_PORT=8081
 HOST=`cat configuration/inventory`
